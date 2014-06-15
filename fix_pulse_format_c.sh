80b6b09408e35c176a9731dc7e0f94385ad45d28
Harrison Chapman <hchaps@gmail.com>
2014-06-15 03:18:12 -0400

    missing -i in sed invocation to save patched file

--- fix_pulse_format_c.sh
+++ fix_pulse_format_c.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if ! grep -q "<bits.h>" pulseaudio/src/pulse/format.c ; then
-	sed 's/<json.h>/<json.h>\n#include <bits.h>/' pulseaudio/src/pulse/format.c
+	sed -i 's/<json.h>/<json.h>\n#include <bits.h>/' pulseaudio/src/pulse/format.c
 fi
